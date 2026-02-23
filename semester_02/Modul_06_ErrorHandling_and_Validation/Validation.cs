5852c93f3cae710f4ffab4225422b9dd643abf5d
Patrick B <p.blume@protonmail.com>
2026-02-23 16:46:00 +0100

    Cleaup gitignore and add ValidationResult

---
+++ semester_02/Modul_06_ErrorHandling_and_Validation/Validation.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Semester2.Parsing;
+
+public struct ValidationResult
+{
+    public bool Ok {get; private set;}
+    public string ErrorCode {get; private set;}
+    public string Message {get; private set;}
+
+    public ValidationResult (bool ok, string error, string message)
+    {
+        if (!ok && string.IsNullOrWhiteSpace(error))
+        {
+            throw new ArgumentException (nameof(error), "Error Code fehlt.");
+        }
+
+        Ok = ok;
+        ErrorCode = error ?? "NONE";
+        Message = message ?? "";
+    }
+    public static ValidationResult Success()
+        {
+            return new ValidationResult(true, "NONE", "SUCCESS");
+        }
+
+    public static ValidationResult Failure(string code, string msg)
+        {
+            return new ValidationResult(false, code, msg);
+        }        
+}
